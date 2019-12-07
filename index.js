aea046edd6421ebbfa5f4884bd0d3adffb25447a
delphinus <me@delphinus.dev>
2019-12-07 21:44:59 +0900

    Add helloApp with Express app

--- index.js
+++ index.js
@@ -1,3 +1,16 @@
+const express = require('express')
+
 exports.helloWorld = (req, res) => {
   res.status(200).send('Hello, World!')
 }
+
+const helloApp = express()
+
+helloApp.get('/morning', (req, res) => {
+  res.status(200).send('Good morning!')
+})
+helloApp.get('/users/:id', (req, res) => {
+  res.status(200).send(`Hello, User: ${req.params.id}`)
+})
+
+exports.helloApp = helloApp
